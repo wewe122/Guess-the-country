a965efeef9b7a70ca64be30b6fb04a6313874477
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2021-11-16 01:44:34 +0200

    first

---
+++ Assets/Quiz/Scripts/QuizManager.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    [SerializeField] private QuizGameUI quizGameUI;
+    [SerializeField] private List<QuizDataScriptable> quizDataList;
+    [SerializeField] private float timeInSeconds;
+
+    private string currentCategory = "";
+    private int correctAnswerCount = 0;
+    private List<Question> questions;
+    //current question data
+    private Question selectedQuetion = new Question();
+    private int gameScore;
+    private int lifesRemaining;
+    private float currentTime;
+    private QuizDataScriptable dataScriptable;
+
+    private GameStatus gameStatus = GameStatus.NEXT;
+
+    public GameStatus GameStatus { get { return gameStatus; } }
+
+    public List<QuizDataScriptable> QuizData { get => quizDataList; }
+
+    public void StartGame(int categoryIndex, string category)
+    {
+        currentCategory = category;
+        correctAnswerCount = 0;
+        gameScore = 0;
+        lifesRemaining = 3;
+        currentTime = timeInSeconds;
+        //set the questions data
+        questions = new List<Question>();
+        dataScriptable = quizDataList[categoryIndex];
+        questions.AddRange(dataScriptable.questions);
+        //select the question
+        SelectQuestion();
+        gameStatus = GameStatus.PLAYING;
+    }
+
+    /// <summary>
+    /// Method used to randomly select the question form questions data
+    /// </summary>
+    private void SelectQuestion()
+    {
+        int val = UnityEngine.Random.Range(0, questions.Count);
+        //set the selectedQuetion
+        selectedQuetion = questions[val];
+        //send the question to quizGameUI
+        quizGameUI.SetQuestion(selectedQuetion);
+
+        questions.RemoveAt(val);
+    }
+
+    private void Update()
+    {
+        if (gameStatus == GameStatus.PLAYING)
+        {
+            currentTime -= Time.deltaTime;
+            SetTime(currentTime);
+        }
+    }
+
+    void SetTime(float value)
+    {
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        quizGameUI.TimerText.text = time.ToString("mm':'ss");   
+
+        if (currentTime <= 0)
+        {
+            //Game Over
+            GameEnd();
+        }
+    }
+
+    /// <summary>
+    /// Method called to check the answer is correct or not
+    /// </summary>
+    /// <param name="selectedOption">answer string</param>
+    /// <returns></returns>
+    public bool Answer(string selectedOption)
+    {
+        bool correct = false;
+        if (selectedQuetion.correctAns == selectedOption)
+        {
+            //Yes, Ans is correct
+            correctAnswerCount++;
+            correct = true;
+            gameScore += 50;
+            quizGameUI.ScoreText.text = "Score:" + gameScore;
+        }
+        else
+        {
+            //No, Ans is wrong
+            lifesRemaining--;
+            quizGameUI.ReduceLife(lifesRemaining);
+
+            if (lifesRemaining == 0)
+            {
+                GameEnd();
+            }
+        }
+
+        if (gameStatus == GameStatus.PLAYING)
+        {
+            if (questions.Count > 0)
+            {
+                //call SelectQuestion method again after 1s
+                Invoke("SelectQuestion", 0.4f);
+            }
+            else
+            {
+                GameEnd();
+            }
+        }
+        return correct;
+    }
+
+    private void GameEnd()
+    {
+        gameStatus = GameStatus.NEXT;
+        quizGameUI.GameOverPanel.SetActive(true);
+
+    }
+}
+
+//Datastructure for storeing the quetions data
+[System.Serializable]
+public class Question
+{
+    public string questionInfo;         //question text
+    public Sprite questionImage;        //for Image
+    public List<string> options;        //options to select
+    public string correctAns;           //correct option
+}
+
+[System.Serializable]
+
+[SerializeField]
+public enum GameStatus
+{
+    PLAYING,
+    NEXT
+}
