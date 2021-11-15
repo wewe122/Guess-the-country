bb6b4a3043243ff9a606119ce4f7c5819387d9f2
avichai1221 <57321080+avichai1221@users.noreply.github.com>
2021-11-16 01:45:13 +0200

    try

---
+++ Assets/Quiz/Scripts/CategoryBtnScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CategoryBtnScript : MonoBehaviour
+{
+    [SerializeField] private Text categoryTitleText;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Button btn;
+
+    public Button Btn { get => btn; }
+
+    public void SetButton(string title, int totalQuestion)
+    {
+        categoryTitleText.text = title;
+        scoreText.text = ""+totalQuestion; 
+    }
+
+}
